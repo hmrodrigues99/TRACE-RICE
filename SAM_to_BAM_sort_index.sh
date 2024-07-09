d8b8266f9d6f009736ed06a141c4391eb103793c
Hugo Rodrigues <108523049+hmrodrigues99@users.noreply.github.com>
2024-07-09 15:58:07 +0100

    Update SAM_to_BAM_sort_index.sh

--- SAM_to_BAM_sort_index.sh
+++ SAM_to_BAM_sort_index.sh
@@ -2,9 +2,14 @@
 ```
 ---------------------------------------------------------
 
-Script name: SAM_to_BAM_sort_index.sh
+Script name: SAM_to_BAM_sort_dups_index.sh
 
-Purpose of the script: Provices options to convert SAM to BAM format, sorting and indexing.
+Purpose of the script: Performs if set to TRUE:
+                       1) Convert SAM to BAM format
+                       2) Add mate score tags
+                       3) Sorting
+                       4) Mark duplicates
+                       4) Indexing
 
 Author: Beatriz Vieira
 
@@ -33,9 +38,11 @@ samtools (v1.7)
 
 # Change to FALSE to disable option(s)
 SAM_to_BAM=TRUE
+BAM_mark_duplicates=TRUE
 BAM_sort=TRUE
 BAM_index=TRUE
 
+# For every sample (n align_variety directories with one variety.sam file each)
 # -------------------------------------------------------
 
 while read f
@@ -43,6 +50,7 @@ while read f
 do
 
 # Convert SAM to BAM files
+# -------------------------------------------------------
 if [ $SAM_to_BAM == TRUE ]
 then
 
@@ -56,12 +64,24 @@ echo ${cmd}
 $cmd
 fi
 
+# Add mate score tags to mark duplicates later
+# -------------------------------------------------------
+if [ $BAM_mark_duplicates == TRUE ]
+then
+
+cmdfix="samtools fixmate -m align_${variety}/${variety}.bam align_${variety}/${variety}.fix.bam"
+echo ${cmdfix}
+$cmdfix
+echo "fixmate for ${variety} worked"
+fi
+
 # Sort BAM files
+# -------------------------------------------------------
 if [ $BAM_sort == TRUE ]
 then
 
-inbam=${variety}.bam
-outsorted=${variety}.sorted
+inbam=${variety}.fix.bam
+outsorted=${variety}.sorted.bam
 
 echo "sorting ${inbam} into ${outsorted}"
 
@@ -70,7 +90,20 @@ echo ${cmdsort}
 $cmdsort
 fi
 
+# Mark duplicates in BAM files
+# -------------------------------------------------------
+if [ $BAM_mark_duplicates == TRUE ]
+then
+
+cmdmark="samtools markdup -s align_${variety}/${variety}.sorted.bam align_${variety}/${variety}_dedup.bam"
+echo $cmdmark
+$cmdmark
+echo  "markdup for $variety worked"
+
+fi
+
 # Index BAM files
+# -------------------------------------------------------
 if [ $BAM_index == TRUE ]
 then
 
